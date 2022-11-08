07ef5ed78510a6c5f8f280ff6aa8c99e60bae8eb
René Tronsgaard <rtr@space.dtu.dk>
2022-11-08 18:23:33 +0100

    Custom order trace plot

--- songpipe.py
+++ songpipe.py
@@ -117,6 +117,28 @@ def mean_combine(images):
     data = [im.data for im in tqdm(images)]
     return Image(data=np.mean(data, axis=0))
 
+"""
+METHODS FOR PLOTTING PYREDUCE OUTPUT
+"""
+def plot_order_trace(flat, orders, column_range, savename=None):
+    fig = plt.figure(figsize=(15, 15))
+    ax = plt.gca()
+
+    vmin = np.quantile(flat, .001)
+    vmax = np.quantile(flat, .85)
+    ax.imshow(flat, vmin=vmin, vmax=vmax, cmap='viridis')
+    ax.invert_yaxis()
+
+    nord = len(orders)
+    for i in range(nord):
+        x = np.arange(*column_range[i])
+        ycen = np.polyval(orders[i], x).astype(int)
+        ax.plot(x, ycen, linestyle='--', color='magenta', linewidth=.5)
+
+    # Save figure to file
+    if savename is not None:
+        fig.savefig(savename, dpi=150)
+
 
 """
 CLASS DEFINITIONS
