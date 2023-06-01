f7400c64ca8034f9ca29e258787be7dd4efdc6e8
René Tronsgaard <rtr@space.dtu.dk>
2023-06-02 00:15:24 +0100

    Remove hardcoded sum extract

--- songpipe.py
+++ songpipe.py
@@ -262,9 +262,6 @@ class CalibrationSet():
         self.steps['norm_flat'] = step_normflat
 
     def extract(self, image, savedir=None):
-        self.config['science']['collapse_function'] = 'sum' 
-        self.config['science']['extraction_method'] = 'arc'  # SIMPLE EXTRACTION TO SPEED UP THINGS
-
         step_science = ScienceExtraction(*self.step_args, **self.config['science'])
         self.steps['science'] = step_science
         orig_filename = basename(image.filename)
