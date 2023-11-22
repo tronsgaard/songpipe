a7cd39f7c635bb844a434ce5c817822102d4ed0f
René Tronsgaard <rtr@space.dtu.dk>
2023-11-22 13:32:36 +0000

    Proper observatory identification

--- songpipe/frame.py
+++ songpipe/frame.py
@@ -4,6 +4,9 @@ import astropy.io.fits as fits
 from . import config
 from .misc import construct_filename
 
+import logging
+logger = logging.getLogger(__name__)
+
 """
 This module contains the Frame and FrameList classes, on which the Image/ImageList and Spectrum/SpectrumList classes are based.
 """
@@ -67,6 +70,7 @@ class Frame:
 
     @property
     def mjd_mid(self):
+        """Get MJD_MID from header or calculate from exptime"""
         try:
             return self.get_header_value('MJD-MID')
         except KeyError:
@@ -76,10 +80,33 @@ class Frame:
     @property
     def type(self):
         return self.get_header_value('IMAGETYP')
+    
+    @property
+    def observatory(self):
+        """Get observatory name from FITS header"""
+        try:
+            return self.get_header_value('OBSERVAT')
+        except KeyError:
+            logger.warning(f'Missing keyword OBSERVAT: {self.filename}')
+            return 'Unknown'
+
+    @property
+    def is_australia(self):
+        """Check if this frame comes from Australia"""
+        return True if self.observatory == 'Mt. Kent' else False
+    
+    @property
+    def is_tenerife(self):
+        """Check if this frame comes from Tenerife"""
+        return True if self.observatory == 'Observatorio del Teide' else False
 
     @property
     def mode(self):
-        """Returns the instrument mode, currently (MtKent): F1, F2, or F12, SLIT, DARK, UNKNOWN"""
+        """
+        Determine the instrument mode: 
+           Mt. Kent:    F1, F2, F12, SLIT, DARK, UNKNOWN
+           Tenerife:    SLITx, DARK, UNKNOWN
+        """
         try:
             # First look for SONGPIPE header keyword
             return self.get_header_value('PL_MODE')
@@ -88,11 +115,11 @@ class Frame:
             if self.type in ('DARK', 'BIAS'):
                 return 'DARK'
             # Tenerife:
-            if self.get_header_value('TELESCOP') == 'Node 1':
+            if self.is_tenerife:
                 slit = self.get_header_value('SLIT')
                 return f'SLIT{slit}'
             # Australia:
-            if self.get_header_value('OBSERVAT') == 'Mt. Kent':
+            if self.is_australia:
                 if self.type == 'FLAT' and self.get_header_value('LIGHTP') == 1:
                     return 'SLIT'
                 if self.type in ('FLAT', 'FLATI2', 'THAR', 'FP'):
@@ -105,12 +132,17 @@ class Frame:
                         return 'F1'
                     if tel2 == 1:
                         return 'F2'
-            # In any other case (including all observations):
-            return 'UNKNOWN'  
-            # FIXME: Figure out a way to detect if there is light in both fibres
+            # In any other case (all observations):
+            # Note: This includes all star spectra from Mt. Kent, as the header 
+            #       doesn't tell if there is starlight in both fibres.
+            return 'UNKNOWN'
 
     # Misc
     def construct_filename(self, **kwargs):
+        """
+        Wrapper for the `construct_filename()` method.
+        Passes object name from FITS header, if config.OBJECT_IN_FILENAME is True
+        """
         if 'object' in kwargs:
             obj = kwargs.pop('object')  # Removes item from kwargs
         elif config.OBJECT_IN_FILENAME is True:
