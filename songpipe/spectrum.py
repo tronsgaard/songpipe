3bf3729ad564bba7e5327339ccb3a910c58ada4a
René Tronsgaard <rtr@space.dtu.dk>
2024-01-05 02:53:38 +0000

    Update spectrum.py

--- songpipe/spectrum.py
+++ songpipe/spectrum.py
@@ -1,8 +1,13 @@
 """Class definitions for extracted spectra"""
 from glob import glob
 from tqdm import tqdm
+import astropy.io.fits as fits
+
 from .frame import Frame, FrameList
 from .misc import apply_limit
+
+import logging
+logger = logging.getLogger(__name__)
 
 """
 This module contains the `Spectrum` class, which is currently identical with the `Frame` class.
@@ -10,13 +15,62 @@ This module contains the `Spectrum` class, which is currently identical with the
 
 class Spectrum(Frame):
     
-    @property
-    def nord(self):
-        pass
+    def __init__(self, header=None, filename=None):
+        self._header = header
+        self._theader = None  # BinTable header (ext 1)
+        self._tdata = None  # BinTable data
+        self.filename = filename
+        self.parent = None  # Not in use for extracted spectra
+        self.file_handle = None
+
+        # Load header from file
+        if self._header is None:
+            with fits.open(filename) as h:
+                self._header = h[0].header  # Don't load data yet
+
+        # Create empty header if necessary
+        if self._header is None:
+            self._header = fits.Header()
+
+    def load_data(self, load_tdata=True):
+        """Load BinTable data containing the spectrum etc."""
+        logger.debug(f'Opening FITS BinTable from "{self.filename}"')
+        with fits.open(self.filename) as h:
+            logger.debug(f'Loading BinTable header')
+            self._theader = h[1].header
+            if load_tdata is True:
+                logger.debug(f'Loading BinTable data')
+                self._tdata = h[1].data
+
+    def clear_data(self, keep_thead=False):
+        """Clear cached data"""
+        self._tdata = None 
+        if keep_thead is False:
+            self._theader = None
 
     @property
-    def npix(self):
-        pass
+    def shape(self):
+        """Get the dimensions (ncol x nord) from BinTable header"""
+        if self._theader is None:
+            self.load_data(load_tdata=False)
+        i = 1
+        while i < 100:
+            try:
+                ttype = self._theader[f'TTYPE{i}']
+                if ttype == 'SPEC':
+                    return self._theader[f'TDIM{i}']  # FIXME parse numbers
+                else:
+                    i += 1
+            except KeyError:
+                raise Exception('BinTable does not contain column SPEC')
+
+    @property
+    def ncol(self):
+        return self.shape[0]  # Not working
+
+    @property
+    def nord(self):
+        return self.shape[1]  # Not working
 
     @property
     def spec(self):
